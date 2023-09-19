caeabaea594a31c78625a22621722f205007c683
Ali <aliifarajj@hotmail.com>
2023-09-19 18:48:04 +0300

    delete workout functionality

--- frontend/src/components/Workout.js
+++ frontend/src/components/Workout.js
@@ -1,8 +1,23 @@
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import axios from 'axios';
 
 const Workout = ({id, name, load, rep, set}) => {
 
+    const handleDelete = async() => {
 
+        try {
+            const res = axios.delete(`http://localhost:4000/workout/${id}`)
+            const data = await res.data;
+            console.log(data)
+            window.location.reload();
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleEdit = () => {
+
+    }
 
     return ( 
         <div className="workout">
@@ -11,7 +26,8 @@ const Workout = ({id, name, load, rep, set}) => {
             <p>Reps: {rep}</p>
             <p>Sets: {set}</p>
             {/* <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix:true})}</p> */}
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
+
         </div>
      );
 }
